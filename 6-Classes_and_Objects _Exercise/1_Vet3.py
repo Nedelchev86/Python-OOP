da9cbae2094b2d287bf05e4975f823116618693c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-20 21:11:41 +0200

    Python Classes and Objects
    Python Classes and Objects

---
+++ 6-Classes_and_Objects _Exercise/1_Vet3.py
@@ -0,0 +1,26 @@
+class Vet:
+    animals = []
+    space = 5
+
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name):
+        if len(Vet.animals) >= Vet.space:
+            return "Not enough space"
+        Vet.animals.append(animal_name)
+        self.animals.append(animal_name)
+
+        return f"{animal_name} registered in the clinic"
+
+
+    def unregister_animal(self, animal_name):
+        if animal_name not in self.animals:
+            return f"{animal_name} not in the clinic"
+        Vet.animals.remove(animal_name)
+        self.animals.remove(animal_name)
+        return f"{animal_name} unregistered successfully"
+
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space - len(Vet.animals)} space left in clinic"
