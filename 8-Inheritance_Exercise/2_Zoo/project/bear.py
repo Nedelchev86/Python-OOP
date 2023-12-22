e6fecf502006a13694212a3cac7108c059989f24
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-22 21:13:00 +0200

    Python Inheritance Exercise
    Python Inheritance Exercise

---
+++ 8-Inheritance_Exercise/2_Zoo/project/bear.py
@@ -0,0 +1,6 @@
+from project.mammal import Mammal
+
+
+class Bear(Mammal):
+    def __init(self, name):
+        super().__init__(name)
---
+++ 8-Inheritance_Exercise/2_Zoo/project/gorilla.py
@@ -0,0 +1,6 @@
+from project.mammal import Mammal
+
+
+class Gorilla(Mammal):
+    def __init(self, name):
+        super().__init__(name)
