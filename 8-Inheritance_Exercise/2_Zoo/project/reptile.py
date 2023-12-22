a3230feb8e0e110c6f1b1fc8211376aeda4c0836
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-22 21:13:10 +0200

    Python Inheritance Exercise
    Python Inheritance Exercise

---
+++ 8-Inheritance_Exercise/2_Zoo/project/reptile.py
@@ -0,0 +1,7 @@
+from project.animal import Animal
+
+
+class Reptile(Animal):
+    # pass
+    def __init(self, name):
+        super().__init__(name)
---
+++ 8-Inheritance_Exercise/2_Zoo/project/snake.py
@@ -0,0 +1,6 @@
+from project.reptile import Reptile
+
+
+class Snake(Reptile):
+    def __init(self, name):
+        super().__init__(name)
