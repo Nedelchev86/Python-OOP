e4c457e21deb1251ba09e3b1a457071cdc380590
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-25 22:10:05 +0200

    Python Class And Static Methods
    Python Class And Static Methods

---
+++ 11-Class_and_Static_Methods_Lab/2_Shop2.py
@@ -0,0 +1,29 @@
+class Shop:
+    def __init__(self, name, type_of_shop, capacity):
+        self.name = name
+        self.type = type_of_shop
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name, type_of_shop):
+        return cls(name, type_of_shop, 10)
+
+    def add_item(self, item_name:str):
+        if sum(self.items.values()) < self.capacity:
+            self.items[item_name] = self.items.get(item_name, 0)
+            self.items[item_name] += 1
+            return f"{item_name} added to the shop"
+        return "Not enough capacity in the shop"
+
+    def remove_item(self, item_name: str, amount: int):
+        if item_name not in self.items or self.items[item_name] < amount:
+            return f"Cannot remove {amount} {item_name}"
+
+        self.items[item_name] -= amount
+        if self.items[item_name] == 0:
+            del self.items[item_name]
+        return f"{amount} {item_name} removed from the shop"
+
+    def __repr__(self):
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
