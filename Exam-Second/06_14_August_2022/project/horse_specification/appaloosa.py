8130f486cfa815642bf2f65116f636e1768e3677
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-01-08 18:42:16 +0200

    Python OOP Exam
    Python OOP Exam

---
+++ Exam-Second/06_14_August_2022/project/horse_specification/appaloosa.py
@@ -0,0 +1,20 @@
+from project.horse_specification.horse import Horse
+
+
+class Appaloosa(Horse):
+    MAX_SPEED = 120
+
+    def __init__(self, name: str, speed: int):
+        super().__init__(name, speed)
+
+    def train(self):
+        if self.speed + 2 > self.MAX_SPEED:
+            self.speed = self.MAX_SPEED
+        else:
+            self.speed += 2
+
+    # def train(self):
+    #     try:
+    #         self.speed += 2
+    #     except ValueError:
+    #         self.speed = self.MAX_SPEED
