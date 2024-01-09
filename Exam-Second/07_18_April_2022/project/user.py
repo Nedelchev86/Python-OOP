9380a5f78d8e1339607c8dff6c9242f9d897b751
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-01-09 20:26:10 +0200

    Python OOP Exam
    Python OOP Exam

---
+++ Exam-Second/07_18_April_2022/project/user.py
@@ -0,0 +1,41 @@
+class User:
+    def __init__(self, username: str, age: int):
+        self.username = username
+        self.age = age
+        self.movies_liked = []
+        self.movies_owned = []
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if not value:
+            raise ValueError("Invalid username!")
+        self.__username = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value < 6:
+            raise ValueError("Users under the age of 6 are not allowed!")
+        self.__age = value
+
+    def __str__(self):
+        result = []
+        result.append(f"Username: {self.username}, Age: {self.age}")
+        result.append("Liked movies:")
+        if len(self.movies_liked) > 0:
+            [result.append(m.details()) for m in self.movies_liked]
+        else:
+            result.append("No movies liked.")
+        result.append("Owned movies:")
+        if len(self.movies_owned) > 0:
+            [result.append(m.details()) for m in self.movies_owned]
+        else:
+            result.append("No movies owned.")
+        return "\n".join(result)
