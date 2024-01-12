f688ac0965e8a9b60e2db28429d074dc80779e54
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-01-12 19:25:27 +0200

    Python OOP Exam
    Python OOP Exam

---
+++ Exam_FIRST_TRY/10_ Encapsulation_Exercise/1_Wild_Cat_Zoo/project/zoo.py
@@ -0,0 +1,36 @@
+class Zoo:
+    def __init__(self, name, budget, animal_capacity, workers_capacity):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+        self.animals = []
+        self.workers = []
+
+    def add_animal(self, animal, price):
+        if self.__budget < price:
+            return "Not enough budget"
+        if self.__animal_capacity == len(self.animals):
+            return "Not enough space for animal"
+
+        self.animals.append(animal)
+        self.__budget -= price
+        return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
+
+    def hire_worker(self, worker):
+        if self.__workers_capacity == len(self.workers):
+            return f"Not enough space for worker"
+        self.workers.append(worker)
+        return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+
+    def fire_worker(self, worker_name):
+        if worker_name in self.workers:
+            self.workers.remove(worker_name)
+            return f"{worker_name} fired successfully"
+        return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        pass
+
+
+
