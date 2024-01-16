083992fb502ab288e63f414c158fc1c1f6800ec4
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-01-16 19:46:48 +0200

    Python OOP Exam
    Python OOP Exam

---
+++ Exam_FIRST_TRY/4_First Steps in OOP - Exercise/8_Pokemon_Battle/project/trainer.py
@@ -0,0 +1,13 @@
+# from pokemon import Pokemon
+#
+#
+# class Trainer:
+#     def __init__(self, name):
+#         self.name = name
+#         self.pokemons = []
+#
+#     def add_pokemon(self, pokemon: Pokemon):
+#         if pokemon not in self.pokemons:
+#             self.pokemons.append(pokemon)
+#         return Pokemon.pokemon_details(self.name,)
+#
